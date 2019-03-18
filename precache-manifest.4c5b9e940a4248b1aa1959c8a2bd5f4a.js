d8ae36624b35c6f2013b984728523786e313bb17
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-18 15:35:42 +0100

    Updates

---
+++ precache-manifest.4c5b9e940a4248b1aa1959c8a2bd5f4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d46b1a377267e1cca668",
+    "url": "/static/css/main.f94cc87b.chunk.css"
+  },
+  {
+    "revision": "d46b1a377267e1cca668",
+    "url": "/static/js/main.c5e9bac3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b1baa83b6e1001fd92a1",
+    "url": "/static/js/2.bf59f247.chunk.js"
+  },
+  {
+    "revision": "5fc0fc42ae309bdd1735076544dce58d",
+    "url": "/index.html"
+  }
+];
